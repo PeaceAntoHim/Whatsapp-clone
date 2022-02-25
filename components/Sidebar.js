248b535a3ea6ac607f02893cf92fdc40e4b482f1
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-02-26 00:14:26 +0700

    added new style

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { Avatar } from '@material-ui/core';
+import { Avatar, IconButton } from '@material-ui/core';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 
@@ -10,8 +10,12 @@ function Sidebar() {
                 <UserAvatar />
 
                 <IconsContainer>
-                    <ChatIcon />
-                    <MoreVertIcon />
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
                 </IconsContainer>
             </Header>
         </Container>
@@ -22,7 +26,18 @@ export default Sidebar;
 
 const Container = styled.div``;
 
-const Header = styled.div``;
+const Header = styled.div`
+    display: flex;
+    position: sticky;
+    top: 0;
+    background-color: white;
+    z-index: 1;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px;
+    height: 80px;
+    border-bottom: 1px solid whitesmoke;
+`;
 
 const UserAvatar = styled(Avatar)`
     margin: 10px;
