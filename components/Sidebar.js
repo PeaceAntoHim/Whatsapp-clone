8b18dec2aa88388b3efda35fbbd1cf26d83b3aa6
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-02-26 10:24:15 +0700

    Building the sidebar components

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
-import { Avatar, IconButton } from '@material-ui/core';
 import ChatIcon from '@material-ui/icons/Chat';
+import SearchIcon from '@material-ui/icons/Search'
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { Avatar, Button ,IconButton } from '@material-ui/core';
 
 function Sidebar() {
     return (
@@ -18,6 +19,13 @@ function Sidebar() {
                     </IconButton>
                 </IconsContainer>
             </Header>
+
+            <Search>
+                <SearchIcon />
+                <SearchInput placeholder="Search in chats" />
+            </Search>
+
+            <SidebarButton>Start new chat</SidebarButton>
         </Container>
     )    
 }
@@ -25,6 +33,30 @@ function Sidebar() {
 export default Sidebar;
 
 const Container = styled.div``;
+
+const Search = styled.div`
+    display: flex;
+    align-items: center;
+    padding: 20px;
+    border-radius: 2px;
+`;
+
+const SidebarButton = styled(Button) `
+    width: 100%;
+
+    &&& {
+        border-top: 1px solid whitesmoke;
+        border-bottom: 1px solid whitesmoke;
+    }
+`;
+
+
+const SearchInput = styled.input`
+    outline-width: 0;
+    border: none;
+    flex: 1;
+`;
+
 
 const Header = styled.div`
     display: flex;
@@ -40,7 +72,11 @@ const Header = styled.div`
 `;
 
 const UserAvatar = styled(Avatar)`
-    margin: 10px;
+    cursor: pointer;
+
+    :hover {
+        opacity: 0.8;
+    }
 `;
 
 const IconsContainer = styled.div`
